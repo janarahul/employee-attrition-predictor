10627ef65c3db0d4c57e43b1aa1c03c5cec597f3
Rahul Yedida <yrahul3910@gmail.com>
2017-02-15 22:11:42 +0530

    Added cx_Freeze setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+import sys
+import cx_Freeze
+from cx_Freeze import setup, Executable
+
+files = [cx_Freeze.Executable("ClassifierWrapper.py"),
+                cx_Freeze.Executable("Classify.py")]
+base = None
+setup(name="calculus",
+      version="1.0",
+      description="",
+      executables=files)
